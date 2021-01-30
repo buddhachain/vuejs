8944490e2f83e6169de4639b9ca6d5921a4508b5
yzKun <1044740162@qq.com>
2021-01-30 23:53:31 +0800

    Complete the details page and start filling in the information page

---
+++ pages/app/activity/editor.vue
@@ -0,0 +1,56 @@
+<template>
+    <view class="editor-page-cont">
+        <view class="add_modal bg-white radius pd-20 _flex">
+            <view class="_icon">
+                <view class="">+</view>
+            </view>
+            <view class="_text">
+                <view class="_title">添加功德主</view>
+                <view class="_info">
+                    <text class="gray">功德主资料将用于记录与反馈</text>
+                </view>
+            </view>
+        </view>
+        <view class="form_modal bg-white radius pd-20">
+            <view class="form">
+                <view class="form-group">
+                    <view class="label">
+                        <view>
+                            <text>功德芳名</text>
+                            <text class="required">*</text>
+                        </view>
+                        <text class="icon">ICON</text>
+                    </view>
+                    <Input placeholder="请填写功德主姓名" bg="white" :radius="true"/>
+                </view>
+                <view class="form-group">
+                    <view class="label">
+                        <view>
+                            <text>功德芳名</text>
+                            <text class="required">*</text>
+                        </view>
+                        <text class="icon">ICON</text>
+                    </view>
+                    <Input placeholder="请填写功德主姓名" bg="white" :radius="true"/>
+                </view>
+            </view>
+        </view>
+    </view>
+</template>
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    },
+    onLoad () {
+         uni.setNavigationBarTitle({
+            title: '佐仓罗布林寺-绿度母烟供法...'
+        });
+    }
+}
+</script>
+<style lang="scss" scoped>
+    @import "../../../styles/app/editor.scss";
+</style>
--- pages/app/activity/main.vue
+++ pages/app/activity/main.vue
@@ -56,7 +56,7 @@
                         </view>
                         <view class="photos">
                             <view>
-                                <view class="_item">
+                                <view class="_item" @click="previewImg(0)">
                                     <image src="../../../static/pages/app/banner.png" mode="" />
                                 </view>
                                 <view class="_item">
@@ -114,6 +114,56 @@
                 </TimeLine>
             </view>
         </view>
+        <view class="trends_modal bg-white radius">
+            <view class="tab_wrap">
+                <view class="tab_items" v-for="(tab, i) in trendsTabList" :key="i" @click="trendsTabIndex = i" :class="{'active': trendsTabIndex === i}">
+                    <text :class="{ gray: trendsTabIndex !== i }">{{ tab }}</text>
+                </view>
+            </view>
+            <view class="list">
+                <view class="list-item" :class="{_open: isOpen}">
+                    <view class="_top _flex">
+                        <view class="_wrap _flex">
+                            <view class="img_box">
+                                <image src="../../../static/pages/app/banner.png" mode="" />
+                            </view>
+                            <view class="_u_name">用户1111</view>
+                        </view>
+                        
+                        <view class="_right">
+                            <view class="_quantity align-right">108 BUD</view>
+                            <view class="_time align-right">
+                                <text class="gray" v-if="trendsTabIndex === 0">01-11 03:10</text>
+                            </view>
+                        </view>
+                    </view>
+                    <view class="_body">
+                        <view class="_msg" v-if="trendsTabIndex === 0">手术完美恢复 容颜貌美 消除鬼魅 心想事成</view>
+                        <!-- hidden -->
+                        <template v-if="isOpen">
+                            <view class="hidden_msg _msg">
+                                <view class="_time">
+                                    <text class="gray">01-12 03:10</text>
+                                </view>
+                                <text>手术完美恢复 容颜貌美 消除鬼魅 心想事成</text>
+                            </view>
+                            <view class="hidden_msg _msg">
+                                <view class="_time">
+                                    <text class="gray">01-12 03:10</text>
+                                </view>
+                                <text>手术完美恢复 容颜貌美 消除鬼魅 心想事成</text>
+                            </view>
+                        </template>
+                         <view class="_open align-right" v-if="trendsTabIndex === 0">
+                            <text v-if="!isOpen" @click="isOpen = true">展开 ICON</text>
+                            <text v-else @click="isOpen = false">收起 ICON</text>
+                        </view>
+                        
+                    </view>
+                    
+                </view>
+            </view>
+        </view>
         <view class="page_footer_wrap bg-white">
            <view class="_left">
                <view class="icon">
@@ -122,204 +172,113 @@
                </view>
            </view>
            <view class="_main">
-               <button>他人拜佛一次，替你灭罪一次</button>
+               <button @click="changePopup(true)">他人拜佛一次，替你灭罪一次</button>
            </view>
         </view>
+        <uni-popup ref="popup" type="bottom">
+            <view class="popup_body bg-white">
+                <view class="_header _flex">
+                    <view class="title">
+                        <text class="gray">参与流程：选择类型 - 填写信息 - 参与成功</text>
+                    </view>
+                    <view class="close">
+                        <text class="gray" @click="changePopup(false)">X</text>
+                    </view>
+                </view>
+                <view class="types">
+                    <view class="_item" v-for="i in 3" :key="i" @click="curActiveType = i" :class="{active: curActiveType === i}">
+                        <view class="_wrap">
+                            <view class="_info _flex">
+                                <view class="_img">
+                                    <image src="../../../static/pages/app/banner.png" mode="" />
+                                </view>
+                                <view class="_name">
+                                    解脱香+哈达+青稞+朵马
+                                </view>
+                                <view class="_price">
+                                    508 BUD
+                                </view>
+                            </view>
+                            <view class="_action" v-if="curActiveType === i">
+                                <button class="sub" :disabled="type_quantity === 1" @click="actionHandles(-1)">-</button>
+                                <view class="_qty">{{ type_quantity }}</view>
+                                <button class="add" @click="actionHandles(1)">+</button>
+                            </view>
+                        </view>
+                    </view> 
+                </view>
+                <view class="_foot">
+                    <button>下一步</button>
+                </view>
+            </view>
+        </uni-popup>
     </view>
 </template>
 <script>
+import uniPopup from '@/components/uni-popup/uni-popup.vue'
+import uniPopupMessage from '@/components/uni-popup/uni-popup-message.vue'
+import uniPopupDialog from '@/components/uni-popup/uni-popup-dialog.vue'
 export default {
+    components: {
+        uniPopup,
+        uniPopupMessage,
+        uniPopupDialog
+    },
     data () {
         return {
             isShowAuthModal: true,
+            curActiveType: 1,
+            type_quantity: 1,
+            isOpen: false,
+            trendsTabList: ['参与动态', '功德榜'],
+            trendsTabIndex: 0
         }
     },
     onLoad () {
         uni.setNavigationBarTitle({
             title: '佐仓罗布林寺-绿度母烟供法...'
         });
-    }
+    },
+    watch: {
+        curActiveType () {
+            this.type_quantity = 1;
+        }
+    },
+    methods: {
+        actionHandles (num) {
+            console.log(num, 'actionHandles')
+            let _n = this.type_quantity + num;
+            console.log(_n)
+            if (_n === 0) {
+                return
+            }
+            console.log('aaa')
+            this.type_quantity = _n;
+        },
+        previewImg (i) {
+            uni.previewImage({
+                urls: ['../../../static/pages/app/banner.png', '../../../static/pages/app/banner.png', '../../../static/pages/app/banner.png'],
+                longPressActions: {
+                    itemList: ['发送给朋友', '保存图片', '收藏'],
+                    success: function(data) {
+                        console.log('选中了第' + (data.tapIndex + 1) + '个按钮,第' + (data.index + 1) + '张图片');
+                    },
+                    fail: function(err) {
+                        console.log(err.errMsg);
+                    }
+                }
+            });
+        },
+        changePopup (type) {
+            if (type) {
+                this.$refs.popup.open()
+            } else {
+                this.$refs.popup.close()
+            }
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
-    .activity-page-cont {
-        padding: 20upx 30upx;
-        padding-bottom: 120upx;
-        .auth_modal {
-            height: 80upx;
-            margin-bottom: 10upx;
-            background: rgba(202, 216, 230, 100);
-            color: rgba(255, 255, 255, 100);
-            font-size: 28upx;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding-right: 20upx;
-            >view {
-                line-height: 80upx;
-            }
-            .title {
-                flex: 1;
-                text-align: center;
-            }
-            .icon {
-                padding: 0 20upx;
-                color: rgba($color: #000000, $alpha: .34);
-            }
-            .detail {
-                height: 60upx;
-                padding: 0 20upx;
-                background: #fff;
-                color: rgba(202, 216, 230, 100);
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                border-radius: 30upx;
-                font-size: 26upx;
-            }
-        }
-        // banner
-        .banner_item {
-            width: 100%;
-            height: 260upx;
-            image {
-                width: 100%;
-                height: 100%;
-            }
-        }
-        .banner_wrap {
-            margin-bottom: 20upx;
-            .activity_name {
-                font-size: 30upx;
-                line-height: 50upx;
-                padding: 14upx 20upx;
-                border-bottom: 1px solid rgba(236, 235, 235, 100);;
-                .status {
-                    padding: 0 20upx;
-                    margin: 0 20upx;
-                    display: inline-block;
-                    background: rgb(202, 216, 230);
-                    color: rgba(255, 255, 255, 100);
-                    border: 1upx solid rgb(139, 173, 205);
-                    color: rgb(139, 173, 205);
-                    border-radius: 30upx;
-                }
-            }
-            .info {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                padding: 20upx;
-                font-size: 28upx;
-                ._img {
-                    width: 80upx;
-                    height: 80upx;
-                    border-radius: 50%;
-                    overflow: hidden;
-                    image {
-                        width: 100%;
-                        height: 100%;
-                    }
-                }
-                ._to {
-                    font-size: 26upx;
-                }
-                ._center {
-                    flex: 1;
-                    margin-left: 20upx;
-                    ._vis {
-                        font-size: 24upx;
-
-                    }
-                    ._name {
-                        line-height: 46upx;
-                    }
-                }
-            }
-        }
-        .html_node_root {
-            padding: 20upx;
-            margin-bottom: 20upx;
-        }
-        .progress_modal {
-            padding: 0 20upx;
-            .title {
-                font-size: 30upx;
-                line-height: 80upx;
-                border-bottom: 1px solid rgba(236, 235, 235, 100);
-            }
-            .time_lines {
-                padding: 20upx 0;
-                ._content {
-                    font-size: 28upx;
-                    line-height: 46upx;
-                    .open_btn {
-                        font-size: 26upx;
-                        line-height: 1;
-                        padding: 10upx;
-                    }
-                    .photos {
-                        padding: 20upx 0;
-                        >view {
-                            width: 100%;
-                            display: flex;
-                            height: 200upx;
-                            justify-content: flex-start;
-                            align-items: center;
-                            margin-bottom: 20upx;
-                            &:last-of-type {
-                                margin-bottom: 0;
-                            }
-                            image {
-                                width: 100%;
-                                height: 100%;
-                            }
-                            ._item {
-                                height: 100%;
-                                flex: 1;
-                                margin-right: 20upx;
-                                max-width: 200upx;
-                                &:last-child {
-                                    margin-right: none;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        .page_footer_wrap {
-            display: flex;
-            justify-content: space-between;
-            padding-right: 0;
-            ._left {
-                font-size: 26upx;
-                padding-right: 20upx;
-                image {
-                    width: 50upx;
-                    height: 50upx;
-                }
-                >view {
-                    line-height: 1;
-                    color: rgba(113, 146, 185, 100);
-                }
-            }
-            ._main {
-                flex: 1;
-                button {
-                    width: 100%;
-                    height: 100%;
-                    color: #fff;
-                    font-size: 28upx;
-                    border-radius: 0;
-                    margin: 0;
-                    line-height: 80upx;
-                    background-color: rgba(113, 146, 185, 100);
-                    &.button-hover {
-                        background: #9fb5ce;
-                    }
-                }
-            }
-        }
-    }
+   @import "../../../styles/app/activity.scss";
 </style>
